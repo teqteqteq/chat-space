bc0d5951cefa84a50ee69298d92ba475f23bed97
teqteqteq <shakajima@gmail.com>
2016-11-25 22:14:10 +0900

    Create groups new

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,16 @@
 class GroupsController < ApplicationController
   def new
+  @group = Group.new
   end
 
-  def edit
+  def create
+    @group = Group.new(group_params)
+    @group.save
+    redirect_to new_group_path
   end
+
+private
+ def group_params
+   params.require(:group).permit(:name)
+ end
 end
